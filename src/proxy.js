209076718bdb3d3a61bdba4faa9c36719f969556
Eirik Vikanes <eirik.vikanes@gmail.com>
2018-05-26 22:23:00 +0200

    Makes it work.

--- src/proxy.js
+++ src/proxy.js
@@ -8,24 +8,20 @@ const config = require("../config")
 
 const app = express()
 
-const onReq = (proxyReq, req, res) => {
-     console.log(proxyReq.headers)
-     console.log(req)
-     proxyReq.headers = req.headers
-}
-
-const onRes = (proxyRes, req, res) => {
-     console.log(proxyRes.headers)
-}
+//const onReq = (proxyReq, req, res) => {
+//    console.log(proxyReq.headers)
+//    console.log(req)
+//    proxyReq.headers = req.headers
+//
+//
+//const onRes = (proxyRes, req, res) => {
+//    console.log(proxyRes.headers)
+//
 
 const options = {
     target: 'http://127.0.0.1:1235/',
-    onProxyReq: onReq,
-    //onProxyRes: onRes
+    //onProxyReq: onReq,
 }
 
-app.use('/flood/', proxy(options))
-
-
+app.use('/flood', proxy(options))
 app.listen(3000, () => console.log('Reverse proxy listening on port 3000!'))
-
